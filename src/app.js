cfdc7967c69e2696b307248f24953466a48f5109
manu <manuella.linck@gmail.com>
2023-11-16 09:29:38 -0300

    rotas do projeto

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 require('dotenv').config();
+const router = require('./routes/rotas');
 const app = express();
 
 app.set('port', process.env.PORT || 3333);
+app.use('/api', router);
 
 module.exports = app;
